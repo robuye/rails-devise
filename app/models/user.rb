c07168b432b73ec1581adc6cf6827dad378180bc
robuye <rulejczyk@gmail.com>
2014-08-20 11:05:19 +0200

    reproduce Devise error in Rails 4.1.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:username)
+    if login
+      where(conditions).where(['lower(username) = :value', { value: login.downcase }]).first
+    else
+      where(conditions).first #here is the line causing exception
+    end
+  end
 end
